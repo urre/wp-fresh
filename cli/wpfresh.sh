47d38b60a46f48abd2977a98b9712baef61407f7
Urban Sanden <hej@urre.me>
2015-08-24 18:31:06 +0200

    Changed some paths

--- cli/wpfresh.sh
+++ cli/wpfresh.sh
@@ -25,24 +25,24 @@ echo "$(tput setaf 2)✔ MySQL dump OK$(tput sgr0)"
 
 # 3. Copy mysql dump to local machine
 echo "$(tput setaf 4)❑ Copying MySQL dump to local machine...$(tput sgr0)"
-rsync --rsh='ssh' -av --partial user@12.34.456.789:../../var/www/www2.mysite.se/dbdump_mysite.sql /Applications/MAMP/htdocs/mysite/
+rsync --rsh='ssh' -av --partial user@12.34.456.789:../../var/www/www2.mysite.se/dbdump_mysite.sql /projects/mysite/
 echo "$(tput setaf 2)✔ Copied MySQL dump OK$(tput sgr0)"
 
 # 4. Sync media (wp-content/uploads folder)
 echo "$(tput setaf 4)❑ Syncing media (wp-content/uploads folder)...$(tput sgr0)"
-rsync -avz --delete user@12.34.456.789:../../var/www/www2.mysite.se/wp-content/uploads/ /Applications/MAMP/htdocs/mysite/wp-content/uploads/
+rsync -avz --delete user@12.34.456.789:../../var/www/www2.mysite.se/wp-content/uploads/ /projects/mysite/wp-content/uploads/
 echo "$(tput setaf 2)✔ Sync media OK$(tput sgr0)"
 
 # 5. Replace with local dev url (change strings in sed command below)
 echo "$(tput setaf 4)❑ Replacing with local dev url...$(tput sgr0)"
-sed -ie 's/www2.mysite.se/mysite.loc/g' /Applications/MAMP/htdocs/mysite/dbdump_mysite.sql
+sed -ie 's/www2.mysite.se/mysite.loc/g' /projects/mysite/dbdump_mysite.sql
 echo "$(tput setaf 2)✔ Replaced with local dev url OK$(tput sgr0)"
 
 # 6. Import to local database
-mysql -u$DBUSER -p$DBPASS -h $DBHOST $DBNAME < /Applications/MAMP/htdocs/mysite/dbdump_mysite.sql
+mysql -u$DBUSER -p$DBPASS -h $DBHOST $DBNAME < /projects/mysite/dbdump_mysite.sql
 
 # 7. Remove temp files
-rm -rf /Applications/MAMP/htdocs/mysite/dbdump_mysite.sql
-rm -rf /Applications/MAMP/htdocs/mysite/dbdump_mysite.sqle
+rm -rf /projects/mysite/dbdump_mysite.sql
+rm -rf /projects/mysite/dbdump_mysite.sqle
 
 echo "$(tput setaf 2)✔ OK! Everything in sync$(tput sgr0)"
